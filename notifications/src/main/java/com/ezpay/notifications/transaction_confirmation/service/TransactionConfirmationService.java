a93de80183b39a048c90c5978e7628b5d204e5b6
Jai Singh <78266589+Jai3-ui@users.noreply.github.com>
2024-08-16 08:40:27 +0530

    Updated Services

--- notifications/src/main/java/com/ezpay/notifications/transaction_confirmation/service/TransactionConfirmationService.java
+++ notifications/src/main/java/com/ezpay/notifications/transaction_confirmation/service/TransactionConfirmationService.java
@@ -26,33 +26,74 @@ public class TransactionConfirmationService {
 	}
 	
 	/* Check if the user with userId `userId` has completed the transaction with transactionId `transactionId` */
-	public TransactionConfirmation hasCompletedTransactionService(Integer userId,Integer transactionId) {
-		return transactionRepoConfirmation.hasCompletedTransaction(userId, transactionId);
+	public TransactionConfirmation hasCompletedTransactionService(TransactionConfirmation transactionConfirmation) {
+		//return transactionRepoConfirmation.hasCompletedTransaction(userId, transactionId);
+		if(transactionConfirmation!=null) {
+			return transactionRepoConfirmation.hasCompletedTransaction(transactionConfirmation);
+		}
+		else {
+			System.out.print("Null Value");
+			return null;
+		}
 	}
 	
 	/* Check if the user with userId `userId` has enabled notifications */
-	public TransactionConfirmation hasEnabledNotitificationService(Integer userId) {
-		return transactionRepoConfirmation.hasEnabledNotification(userId);
+	public TransactionConfirmation hasEnabledNotitificationService(TransactionConfirmation transactionConfirmation) {
+		if(transactionConfirmation!=null) {
+			return transactionRepoConfirmation.hasEnabledNotification(transactionConfirmation);
+		}
+		else {
+			System.out.println("Null Values");
+			return null;
+		}
+		
 	}
 	
 	/* Getting the Confirmation Message for the Transaction with `transactionId` performed by the User `userId` */
-	public String getConfirmationMessageService(Integer userId,Integer transactionId) {
-		return transactionRepoConfirmation.getConfirmationMessage(userId, transactionId);
+	public String getConfirmationMessageService(TransactionConfirmation transactionConfirmation) {
+		if(transactionConfirmation!=null) {
+			return transactionRepoConfirmation.getConfirmationMessage(transactionConfirmation);
+		}
+		else {
+			System.out.println("Null Values");
+			return null;
+		}
     }
 	
 	/* Getting the Number of Transaction Completed by the User with `userId` */
-	public Integer numCompletedTransactionService(Integer userId) {
-		return transactionRepoConfirmation.numCompletedTransaction(userId);
+	public Integer numCompletedTransactionService(TransactionConfirmation transactionConfirmation) {
+		if(transactionConfirmation!=null) {
+			return transactionRepoConfirmation.numCompletedTransaction(transactionConfirmation);
+		}
+		else {
+			System.out.println("Null Value");
+			return null;
+		}
+		
     }
 	
 	/* Getting all the Transaction Completed by the User with `userId` */
-	public TransactionSummary getTransactionSummaryService(Integer userId){
-		return transactionRepoConfirmation.getTransactionSummary(userId);
+	public TransactionSummary getTransactionSummaryService(TransactionConfirmation transactionConfirmation){
+		if(transactionConfirmation!=null) {
+			return transactionRepoConfirmation.getTransactionSummary(transactionConfirmation);
+		}
+		else {
+			System.out.println("Null Values");
+			return null;
+		}
+		
 	}
 	
 	/* Check whether the User with `userId` received the Notification or not */
-	public boolean hasReceivedNotificationService(Integer userId,Integer transactionId) {
-		return transactionRepoConfirmation.hasReceivedNotification(userId, transactionId);
+	public String hasReceivedNotificationService(TransactionConfirmation transactionConfirmation) {
+		if(transactionConfirmation!=null) {
+			return transactionRepoConfirmation.hasReceivedNotification(transactionConfirmation);
+		}
+		else {
+			System.out.println("Null Values");
+			return null;
+		}
+		
 	}	
 
 }
