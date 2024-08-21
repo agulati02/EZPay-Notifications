13448569690606bba81ce362fc7becdc3fe83f09
doneela <doneeladas@gmail.com>
2024-08-21 20:25:14 +0530

    Updated DBMS methods in PaymentReminderRepo

--- notifications/src/main/java/com/ezpay/notifications/repo/PaymentReminderRepo.java
+++ notifications/src/main/java/com/ezpay/notifications/repo/PaymentReminderRepo.java
@@ -1,5 +1,8 @@
 package com.ezpay.notifications.repo;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -7,27 +10,21 @@ import java.util.concurrent.TimeUnit;
 import com.ezpay.notifications.model.PaymentReminder;
 
 /**
- * A dummy data repository for all payment reminders.
+ * A repository for payment reminders using JDBC.
  * 
- * Author: Anurag Gulati
- * Date: 11.08.2024
+ * Author: Doneela Das
+ * Date: 20.08.2024
  */
-@SuppressWarnings("deprecation")
+
+
+//@SuppressWarnings("deprecation")
 public class PaymentReminderRepo {
+	private DBConnectionPaymentReminder dbConnection;
 
-	private ArrayList<PaymentReminder> listOfPaymentReminders;
+    public PaymentReminderRepo() {
+        this.dbConnection = new DBConnectionPaymentReminder();
+    }
 
-	{
-		this.listOfPaymentReminders = new ArrayList<PaymentReminder>();
-		this.listOfPaymentReminders
-				.add(new PaymentReminder("R01U01", "U01", 100.0, new Date(2024-1900, 8-1, 12), "Incomplete"));
-		this.listOfPaymentReminders
-				.add(new PaymentReminder("R02U01", "U01", 150.0, new Date(2024-1900, 8-1, 15), "Incomplete"));
-		this.listOfPaymentReminders.add(new PaymentReminder("R11U12", "U12", 50.0, new Date(2024-1900, 8-1, 11), "Complete"));
-		this.listOfPaymentReminders
-				.add(new PaymentReminder("R05U09", "U09", 250.0, new Date(2024-1900, 10-1, 10), "Incomplete"));
-	}
-	
 	/**
 	 * Add a new payment reminder.
 	 * 
@@ -41,12 +38,11 @@ public class PaymentReminderRepo {
 	 * @return (Boolean) A boolean to acknowledge the addition of a payment
 	 *         reminder.
 	 */
-	public boolean addPaymentReminder(String reminderId, String userId, Double amount, Date dueDate, String status) {
-		PaymentReminder paymentReminder = new PaymentReminder(reminderId, userId, amount, dueDate, status);
-		boolean addStatus = this.listOfPaymentReminders.add(paymentReminder);
-		System.out.println(this.listOfPaymentReminders.size());
-		return addStatus;
-	}
+    
+
+    public boolean addPaymentReminder(String reminderId, String userId, Double amount, Date dueDate, String status) {
+        return dbConnection.addPaymentReminder(reminderId, userId, amount, new java.sql.Date(dueDate.getTime()), status);
+    }
 
 	/**
 	 * Delete a payment reminder.
@@ -56,23 +52,12 @@ public class PaymentReminderRepo {
 	 * @return (Boolean) A boolean to acknowledge the deletion of a payment
 	 * reminder.
 	 */
-	
-	public boolean deletePaymentReminder(String reminderId) {
-	    boolean deletionStatus = false;
-	    ArrayList<PaymentReminder> newListOfReminders = new ArrayList<PaymentReminder>();
-	    for (PaymentReminder paymentReminder : this.listOfPaymentReminders) {
-	        if (paymentReminder.getReminderId().equals(reminderId)) {
-	            deletionStatus = true;
-	            continue;
-	        } else {
-	        	newListOfReminders.add(paymentReminder);
-	        }
-	    }
-	    this.listOfPaymentReminders = newListOfReminders;
-	    return deletionStatus;
-	}
+    
+    public boolean deletePaymentReminder(String reminderId) {
+        return dbConnection.deletePaymentReminder(reminderId);
+    }
 
-
+    
 	/**
 	 * Delete all payment reminders corresponding to a user.
 	 * 
@@ -84,20 +69,51 @@ public class PaymentReminderRepo {
 	 * Author : Anurag Gulati
 	 * Date: 11.08.2024
 	 */
-	public Integer deleteAllUserReminders(String userId) {
-		Integer numberOfRemindersDeleted = 0;
-		ArrayList<PaymentReminder> newListOfReminders = new ArrayList<PaymentReminder>();
-		for (PaymentReminder paymentReminder : this.listOfPaymentReminders) {
-			if (paymentReminder.getUserId().equals(userId)) {
-				numberOfRemindersDeleted += 1;
-				continue;
-			} else {
-				newListOfReminders.add(paymentReminder);
-			}
-		}
-		this.listOfPaymentReminders = newListOfReminders;
-		return numberOfRemindersDeleted;
-	}
+
+    public Integer deleteAllUserReminders(String userId) {
+        return dbConnection.deleteAllUserReminders(userId);
+    }
+
+    /**
+     * Fetch all payment reminders for a given user with due date in the past or
+     * within the next 3 days.
+     * 
+     * @param userId: (String) Unique key identify the user.
+     * 
+     * @return (ArrayList<String>) List of reminder messages corresponding to the
+     * given user.
+     */
+        public ArrayList<String> fetchPaymentReminders(String userId) {
+            ArrayList<String> paymentReminders = new ArrayList<>();
+            try (Connection connection = dbConnection.getConnection()) {
+                ResultSet resultSet = dbConnection.fetchPaymentReminders(userId);
+
+                if (resultSet != null) {
+                    while (resultSet.next()) {
+                        String reminderId = resultSet.getString("reminderId");
+                        String user = resultSet.getString("userId");
+                        Double amount = resultSet.getDouble("amount");
+                        Date dueDate = resultSet.getDate("dueDate");
+                        String status = resultSet.getString("status");
+
+                        PaymentReminder paymentReminder = new PaymentReminder(reminderId, user, amount, dueDate, status);
+
+                        Long differenceMilliseconds = paymentReminder.getDueDate().getTime() - (new Date()).getTime();
+                        Long differenceDays = TimeUnit.MILLISECONDS.toDays(differenceMilliseconds) + 1;
+                        if (differenceDays < 3) {
+                            paymentReminders.add(generateMessageFromReminder(paymentReminder));
+                        }
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                dbConnection.closeConnection();
+            }
+            return paymentReminders;
+        }
+    
+
 
 	/**
 	 * (Helper) This method is used to generate a reminder message from the
@@ -107,31 +123,13 @@ public class PaymentReminderRepo {
 	 * 
 	 * @return (String) Reminder text generated from paymentReminder.
 	 */
-	private String generateMessageFromReminder(PaymentReminder paymentReminder) {
-		return "(REM. ID:" + paymentReminder.getReminderId() + ") " + "Payment of Rs. " + String.format("%.2f", paymentReminder.getAmount()) + " due on "
-				+ paymentReminder.getDueDate().toString();
-	}
-
-	/**
-	 * Fetch all payment reminders for a given user.
-	 * 
-	 * @param userId: (String) Unique key identify the user.
-	 * 
-	 * @return (ArrayList<String>) List of reminder messages corresponding to the
-	 * given user.
-	 */
-	public ArrayList<String> fetchPaymentReminders(String userId) {
-		ArrayList<String> paymentReminders = new ArrayList<String>();
-		for (PaymentReminder paymentReminder : this.listOfPaymentReminders) {
-			if (paymentReminder.getUserId().equals(userId)) {
-				Long differenceMilliseconds = paymentReminder.getDueDate().getTime() - (new Date()).getTime();
-				Long differenceDays = TimeUnit.MILLISECONDS.toDays(differenceMilliseconds) + 1;
-				if (differenceDays < 3) {
-					paymentReminders.add(this.generateMessageFromReminder(paymentReminder));
-				}
-			}
-		}
-		return paymentReminders;
-	}
+        
+        private String generateMessageFromReminder(PaymentReminder paymentReminder) {
+            return "(REM. ID:" + paymentReminder.getReminderId() + ") " + "Payment of Rs. " + String.format("%.2f", paymentReminder.getAmount()) + " due on "
+                    + paymentReminder.getDueDate().toString();
+        }
 
 }
+
+
+
