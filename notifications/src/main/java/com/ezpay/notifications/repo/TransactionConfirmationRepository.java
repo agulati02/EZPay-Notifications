d62faa9c13cf927e6c4fb9941a2c04ab70bd33de
Geethaa19 <100697393+Geethaa19@users.noreply.github.com>
2024-08-22 10:11:36 +0530

    Update TransactionConfirmationRepository.java

--- notifications/src/main/java/com/ezpay/notifications/repo/TransactionConfirmationRepository.java
+++ notifications/src/main/java/com/ezpay/notifications/repo/TransactionConfirmationRepository.java
@@ -2,8 +2,9 @@ package com.ezpay.notifications.repo;
 
 /*
  * Author Name: Geethapriya Thandavamurthi  
- * Date Details: 19/08/2024 * Monday 
- * */
+ * Date Details: 19/08/2024 * Monday
+ * /
+ 
 /*
  * Module Description 
  The TransactionConfirmationRepository class serves as a data repository for managing 
@@ -18,19 +19,25 @@ import java.util.List;
 import com.ezpay.notifications.model.TransactionConfirmation;
 import com.ezpay.notifications.model.TransactionSummary;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TransactionConfirmationRepository {
     
+	private static final Logger LOGGER = Logger.getLogger(TransactionConfirmationRepository.class.getName());
+	
     List<TransactionConfirmation> transactionConfirmation;
     
     
     /*
-    userId, hasCompleted, enabledNotification, confMessage, transactionId, hasReceived  
+    userId, hasCompleted, enabledNotification, confirmationMessage, transactionId, hasReceived  
     transactionConfirmation.add(new TransactionConfirmation(1, false, false, "Transaction Incompleted", 2, false));
     transactionConfirmation.add(new TransactionConfirmation(2, true, true, "Transaction Completed", 4, true));
     transactionConfirmation.add(new TransactionConfirmation(3,false,true,"Transaction Incompleted",7,true));
     transactionConfirmation.add(new TransactionConfirmation(1, true, false, "Transaction Completed", 5, false));
     transactionConfirmation.add(new TransactionConfirmation(1, true, false, "Transaction Completed", 15, true));
+    transactionConfirmation.add(new TransactionConfirmation(1, true, false, "Transaction Completed", 10, false));
+    transactionConfirmation.add(new TransactionConfirmation(1, false, false, "Transaction Incompleted",3, false));
     */
     
     
@@ -41,35 +48,38 @@ public class TransactionConfirmationRepository {
     	Integer userId;
     	boolean hasCompletedBool;
     	boolean enabledNotification;
-    	String confMessage;
+    	String confirmationMessage;
     	Integer transactionId;
     	boolean hasReceived;
     	
     	try {
-    		Connection connection = DBConnectionTransactionReminder.getConnection();
+    		Connection connection = DBConnectionTransactionConfirmation.getConnection();
     		
-    		PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()+" AND transaction_id = "+transConfirmation.getTransactionId()); 
+    		PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()+" AND transaction_id = "+transConfirmation.getTransactionId()); 
 
-    		ResultSet resultSet = pstmt.executeQuery();
+    		ResultSet resultSet = preparedStatement.executeQuery();
     		
+    		//if resultSet is not null
     		if (resultSet.next()) {
     			
                int hasCompleted = resultSet.getInt("has_completed");
+               
+               //if hasCompleted is true
                if (hasCompleted == 1) {
+            	   hasCompletedBool= true;
+            	   userId = Integer.valueOf(resultSet.getInt("user_id"));
+            	   enabledNotification = resultSet.getBoolean("enabled_notification");
+            	   confirmationMessage = resultSet.getString("conf_message");
+            	   transactionId = Integer.valueOf(resultSet.getInt("transaction_id"));
+            	   hasReceived = resultSet.getBoolean("has_received");
             	   
-	            	   hasCompletedBool= true;
-	            	   userId = Integer.valueOf(resultSet.getInt("user_id"));
-	            	   enabledNotification = resultSet.getBoolean("enabled_notification");
-	            	   confMessage = resultSet.getString("conf_message");
-	            	   transactionId = Integer.valueOf(resultSet.getInt("transaction_id"));
-	            	   hasReceived = resultSet.getBoolean("has_received");
-	            	   
-	                   result = new TransactionConfirmation(userId, hasCompletedBool, enabledNotification, confMessage, transactionId, hasReceived); 
-               		}
-    			}
-           } catch (Exception e) {
-               e.printStackTrace();
-           }
+                   result = new TransactionConfirmation(userId, hasCompletedBool, enabledNotification, confirmationMessage, transactionId, hasReceived); 
+               	}
+    		}
+    	}catch (Exception e) {
+           e.printStackTrace();
+           LOGGER.log(Level.SEVERE, "Error checking if transaction is completed for userId: " + transConfirmation.getUserId() + " and transactionId: " + transConfirmation.getTransactionId(), e);
+        }
 
            return result;
     }
@@ -80,36 +90,40 @@ public class TransactionConfirmationRepository {
     	
     	Integer userId;
     	boolean hasCompleted;
-    	boolean enabledNotificationBool;
-    	String confMessage;
+    	boolean isNotificationEnabled;
+    	String confirmationMessage;
     	Integer transactionId;
     	boolean hasReceived;
     	
     	try {
-    		Connection connection = DBConnectionTransactionReminder.getConnection();
+    		Connection connection = DBConnectionTransactionConfirmation.getConnection();
     		
-    		PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()); 
+    		PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()); 
 
-    		ResultSet resultSet = pstmt.executeQuery();
+    		ResultSet resultSet = preparedStatement.executeQuery();
     		
+    		//if resultSet is not null
     		if (resultSet.next()) {
     			
                int enabledNotification = resultSet.getInt("enabled_notification");
+               
+               //if enabledNotification is true
                if (enabledNotification == 1) {
             	   
-	            	   enabledNotificationBool= true;
-	            	   userId = Integer.valueOf(resultSet.getInt("user_id"));
-	            	   hasCompleted = resultSet.getBoolean("has_completed");
-	            	   confMessage = resultSet.getString("conf_message");
-	            	   transactionId = Integer.valueOf(resultSet.getInt("transaction_id"));
-	            	   hasReceived = resultSet.getBoolean("has_received");
-	            	   
-	                   result = new TransactionConfirmation(userId, hasCompleted, enabledNotificationBool, confMessage, transactionId, hasReceived); 
-               		}
-    			}
-           } catch (Exception e) {
+            	   isNotificationEnabled= true;
+            	   userId = Integer.valueOf(resultSet.getInt("user_id"));
+            	   hasCompleted = resultSet.getBoolean("has_completed");
+            	   confirmationMessage = resultSet.getString("conf_message");
+            	   transactionId = Integer.valueOf(resultSet.getInt("transaction_id"));
+            	   hasReceived = resultSet.getBoolean("has_received");
+            	   
+                   result = new TransactionConfirmation(userId, hasCompleted, isNotificationEnabled, confirmationMessage, transactionId, hasReceived); 
+               	}
+    		}
+        } catch (Exception e) {
                e.printStackTrace();
-           }
+               LOGGER.log(Level.SEVERE, "Error checking if notifications are enabled for userId: " + transConfirmation.getUserId(), e);
+        }
 
            return result;
     	
@@ -120,30 +134,34 @@ public class TransactionConfirmationRepository {
     	String result = null;
     	
     	try {
-    		Connection connection = DBConnectionTransactionReminder.getConnection();
+    		Connection connection = DBConnectionTransactionConfirmation.getConnection();
     		
-    		PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()+" AND transaction_id = "+transConfirmation.getTransactionId()); 
+    		PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()+" AND transaction_id = "+transConfirmation.getTransactionId()); 
 
-    		ResultSet resultSet = pstmt.executeQuery();
+    		ResultSet resultSet = preparedStatement.executeQuery();
     		
+    		//if resultSet is not null
     		if (resultSet.next()) {
     			
                int hasCompleted = resultSet.getInt("has_completed");
+               
+               //if hasCompleted is true
                if (hasCompleted == 1) {
             	   result = resultSet.getString("conf_message");
             	   return result;
-	            }
+	           }
                else {
             	   result = resultSet.getString("conf_message");
             	   return result;
-                }   
+               }   
     		}
-           } catch (Exception e) {
+        } catch (Exception e) {
                e.printStackTrace();
-           }
+               LOGGER.log(Level.SEVERE, "Error retrieving confirmation message for userId: " + transConfirmation.getUserId() + " and transactionId: " + transConfirmation.getTransactionId(), e);
+        }
     	
     	result = "Transaction not performed by User";
-           return result;
+        return result;
     	
     }
     
@@ -152,39 +170,29 @@ public class TransactionConfirmationRepository {
     	Integer count = 0;
     	
     	try {
-    		Connection connection = DBConnectionTransactionReminder.getConnection();
+    		Connection connection = DBConnectionTransactionConfirmation.getConnection();
     		
-    		PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()); 
+    		PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()); 
 
-    		ResultSet resultSet = pstmt.executeQuery();
+    		ResultSet resultSet = preparedStatement.executeQuery();
     		
+    		//while resultSet is not null
     		while (resultSet.next()) {
     			
                int hasCompleted = resultSet.getInt("has_completed");
+               
+               //if hasCompleted is true
                if (hasCompleted == 1) {
             	   count++;
-	            	} 
-    			}
-           } catch (Exception e) {
-               e.printStackTrace();
-           }
+	           } 
+    		}
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Error counting completed transactions for userId: " + transConfirmation.getUserId(), e);
+        }
     	
     	return count;
     }
-    
-    /*public Integer numCompletedTransaction(TransactionConfirmation transConfirmation) {
-    	Integer count=0;
-    	for(TransactionConfirmation transactionConfirmation:transactionConfirmation) {
-    		// Use equals to compare Integer values
-    		if(transactionConfirmation.getUserId().equals(transConfirmation.getUserId())) {
-    			if(transactionConfirmation.getHasCompleted()) {
-    				count++;
-    			}
-    		}
-    	}
-    	return count;
-    }*/
-    
     
     /* Method to get a summary of completed and incomplete transactions for a user*/
     public TransactionSummary getTransactionSummary(TransactionConfirmation transConfirmation) {
@@ -196,30 +204,37 @@ public class TransactionConfirmationRepository {
     	Integer transactionId;
     	
         try {
-    		Connection connection = DBConnectionTransactionReminder.getConnection();
+    		Connection connection = DBConnectionTransactionConfirmation.getConnection();
     		
-    		PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()); 
+    		PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()); 
 
-    		ResultSet resultSet = pstmt.executeQuery();
+    		ResultSet resultSet = preparedStatement.executeQuery();
     		
+    		//while resultSet is not null
     		while (resultSet.next()) {
     			
                int hasCompleted = resultSet.getInt("has_completed");
+               
+               //if hasCompleted is true
                if (hasCompleted == 1) {
             	   transactionId = Integer.valueOf(resultSet.getInt("transaction_id"));
+            	   //increment completedCount
             	   completedCount++;
             	   completedTransactions.add(transactionId);
 	            }
                else {
             	   transactionId = Integer.valueOf(resultSet.getInt("transaction_id"));
+            	   //increment incompleteCount
             	   incompleteCount++;
             	   incompleteTransactions.add(transactionId);
                }
     		}
         } catch (Exception e) {
-               e.printStackTrace();
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Error retrieving transaction summary for userId: " + transConfirmation.getUserId(), e);
         }
     	
+        //return TransactionSummary
         return new TransactionSummary(completedCount, incompleteCount, completedTransactions, incompleteTransactions);
     }
     
@@ -227,22 +242,26 @@ public class TransactionConfirmationRepository {
     public String hasReceivedNotification(TransactionConfirmation transConfirmation) {
     
     	try {
-    		Connection connection = DBConnectionTransactionReminder.getConnection();
+    		Connection connection = DBConnectionTransactionConfirmation.getConnection();
     		
-    		PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()+" AND transaction_id = "+transConfirmation.getTransactionId()); 
+    		PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM transaction_confirmation WHERE user_id = " + transConfirmation.getUserId()+" AND transaction_id = "+transConfirmation.getTransactionId()); 
 
-    		ResultSet resultSet = pstmt.executeQuery();
+    		ResultSet resultSet = preparedStatement.executeQuery();
     		
+    		//while resultSet is not null
     		while (resultSet.next()) {
     			
                int hasReceived = resultSet.getInt("has_received");
+               
+               //if hasReceived is true
                if (hasReceived == 1) {
             	   return "Received the Notification";
-	            	} 
-    			}
-           } catch (Exception e) {
-               e.printStackTrace();
-           }
+	            } 
+    		}
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Error checking notification receipt for userId: " + transConfirmation.getUserId() + " and transactionId: " + transConfirmation.getTransactionId(), e);
+        }
     	
     	return "Not received the Notification";
     }
