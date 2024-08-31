49cdc9c84d8b9aa3ec5cab64c404fecd0807e781
Akhil kholia <87223030+kholiator@users.noreply.github.com>
2024-09-01 00:56:19 +0530

    Update NotificationRepo.java

--- Notification/Notification/src/main/java/com/example/Notification/repository/NotificationRepo.java
+++ Notification/Notification/src/main/java/com/example/Notification/repository/NotificationRepo.java
@@ -7,8 +7,21 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * NotificationRepo interface
+ * Repository interface for Notification entity, providing CRUD operations.
+ * 
+ * @author Akhil Kholia
+ * @date 2024-08-31
+ */
 @Repository
 public interface NotificationRepo extends JpaRepository<Notification, Long> {
-
-    public List<Notification> findByUserId(Long uId);
+	
+	/**
+     * Finds notifications by user ID.
+     * 
+     * @param userId User ID to search notifications for.
+     * @return List of Notification objects.
+     */
+     List<Notification> findByUserId(Long uId);
 }
