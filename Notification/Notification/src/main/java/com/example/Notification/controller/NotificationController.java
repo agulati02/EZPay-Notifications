23088f57f29c5cca548db7df765f57fe68bbe45e
Akhil kholia <87223030+kholiator@users.noreply.github.com>
2024-09-01 00:53:22 +0530

    Update NotificationController.java

--- Notification/Notification/src/main/java/com/example/Notification/controller/NotificationController.java
+++ Notification/Notification/src/main/java/com/example/Notification/controller/NotificationController.java
@@ -8,20 +8,46 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * NotificationController class Handles REST API requests for notifications and
+ * transactions.
+ * 
+ * @author Akhil Kholia
+ * @date 2024-08-31
+ */
+
 @RestController
 @RequestMapping("/api")
 public class NotificationController {
-    @Autowired
-    NotificationService notificationService;
+	@Autowired
+	NotificationService notificationService;
 
-    @PostMapping("/processTransaction/")
-    public void processTransaction(@RequestBody Transaction transaction){
-        System.out.println(transaction.getId() + "---------" + transaction.getUserId());
-        notificationService.processTransation(transaction);
-        return;
-    }
-    @GetMapping("/notifications/{uid}")
-    public List<Notification> getUserNotification(@PathVariable Long uid){
-        return notificationService.getNotificationsForUser(uid);
-    }
+//    @GetMapping("/test")
+//    public String test(){
+//        return "testing";
+//    }
+
+	/**
+	 * Processes a transaction and triggers notification logic.
+	 * 
+	 * @param transaction Transaction object containing details of the transaction
+	 *                    to process.
+	 */
+	@PostMapping("/processTransaction")
+	public void processTransaction(@RequestBody Transaction transaction) {
+		// Log transaction details for debugging
+//		System.out.println(transaction.getId() + "---------" + transaction.getUserId());
+		notificationService.processTransaction(transaction);
+	}
+
+	/**
+	 * Retrieves notifications for a specific user.
+	 * 
+	 * @param uid User ID for which notifications are fetched.
+	 * @return List of Notification objects.
+	 */
+	@GetMapping("/notifications/{uid}")
+	public List<Notification> getUserNotification(@PathVariable Long uid) {
+		return notificationService.getNotificationsForUser(uid);
+	}
 }
