a6dc247c4eca313f23e227305010c31eda1afde1
Akhil kholia <87223030+kholiator@users.noreply.github.com>
2024-09-01 00:56:51 +0530

    Update TransactionRepo.java

--- Notification/Notification/src/main/java/com/example/Notification/repository/TransactionRepo.java
+++ Notification/Notification/src/main/java/com/example/Notification/repository/TransactionRepo.java
@@ -4,6 +4,15 @@ import com.example.Notification.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * TransactionRepo interface
+ * Repository interface for Transaction entity, providing CRUD operations.
+ * This interface allows for interaction with the database for Transaction-related queries.
+ * 
+ * @author Akhil Kholia
+ * @date 2024-08-31
+ */
 @Repository
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
+	// Using default CRUD operations provided by JpaRepository
 }
