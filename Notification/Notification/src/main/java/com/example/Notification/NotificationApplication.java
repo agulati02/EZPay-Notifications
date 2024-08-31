a157cb81fc154702354429e91469ba58462c455e
Akhil kholia <87223030+kholiator@users.noreply.github.com>
2024-09-01 00:58:28 +0530

    Update NotificationApplication.java

--- Notification/Notification/src/main/java/com/example/Notification/NotificationApplication.java
+++ Notification/Notification/src/main/java/com/example/Notification/NotificationApplication.java
@@ -3,6 +3,13 @@ package com.example.Notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * NotificationApplication class
+ * Main entry point for the Notification Spring Boot application.
+ * 
+ * @author Akhil Kholia
+ * @date 2024-08-31
+ */
 @SpringBootApplication
 public class NotificationApplication {
 
