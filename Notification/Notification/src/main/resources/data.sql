dbd9e15eb523e1d9a211534e3fac371a340a7487
Akhil kholia <87223030+kholiator@users.noreply.github.com>
2024-09-01 00:59:04 +0530

    Update data.sql

--- Notification/Notification/src/main/resources/data.sql
+++ Notification/Notification/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO User (id,Notification_Enabled, User_Name, password, email, Reg_Date) VALUES (1, true, 'akhil', 'lihka', 'akhil@ak.in', '2023-05-08');
-INSERT INTO User (id,Notification_Enabled, User_Name, password, email, Reg_Date) VALUES (2, true, 'xyz', 'oiu', 'xyz@jjj.jj', '2013-05-18');
-INSERT INTO User (id,Notification_Enabled, User_Name, password, email, Reg_Date) VALUES (3, false, 'test', 'tre', 'fffl@ak.in', '2023-09-28');
-INSERT INTO User (id,Notification_Enabled, User_Name, password, email, Reg_Date) VALUES (4, true, 'mock', 'rrr', 'wwww@vv.in', '2010-02-08');
-INSERT INTO User (id,Notification_Enabled, User_Name, password, email, Reg_Date) VALUES (5, false, 'try', 'sss', 'bbb@ak.in', '2020-04-01');
+INSERT INTO User (id,Notifications_Enabled, Username, password, email, registration_Date) VALUES (1, true, 'akhil', 'lihka', 'akhil@ak.in', '2023-05-08');
+INSERT INTO User (id,Notifications_Enabled, Username, password, email, registration_Date) VALUES (2, true, 'xyz', 'oiu', 'xyz@jjj.jj', '2013-05-18');
+INSERT INTO User (id,Notifications_Enabled, Username, password, email, registration_Date) VALUES (3, false, 'test', 'tre', 'fffl@ak.in', '2023-09-28');
+INSERT INTO User (id,Notifications_Enabled, Username, password, email, registration_Date) VALUES (4, true, 'mock', 'rrr', 'wwww@vv.in', '2010-02-08');
+INSERT INTO User (id,Notifications_Enabled, Username, password, email, registration_Date) VALUES (5, false, 'try', 'sss', 'bbb@ak.in', '2020-04-01');
