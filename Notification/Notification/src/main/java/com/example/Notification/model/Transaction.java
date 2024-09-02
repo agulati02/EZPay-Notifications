78e122fe93d9860af448d45fffa30fd2a8bdff10
Geethaa19 <100697393+Geethaa19@users.noreply.github.com>
2024-09-02 12:46:38 +0530

    Update Transaction.java

--- Notification/Notification/src/main/java/com/example/Notification/model/Transaction.java
+++ Notification/Notification/src/main/java/com/example/Notification/model/Transaction.java
@@ -62,9 +62,17 @@ public class Transaction {
     public Long getId() {
         return id;
     }
+    
+    public void setId(Long id) {
+    	this.id = id;
+    }
 
     public Long getUserId() {
         return userId;
+    }
+    
+    public void setUserId(Long userId) {
+    	this.userId = userId;
     }
 
     
