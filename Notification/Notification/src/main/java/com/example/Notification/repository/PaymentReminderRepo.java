ddf8bf485cd4108ffc1490b1ed3d60b5910c0ccd
doneela <doneeladas@gmail.com>
2024-09-03 17:59:41 +0530

    Updated PaymentReminderRepo.java file

--- Notification/Notification/src/main/java/com/example/Notification/repository/PaymentReminderRepo.java
+++ Notification/Notification/src/main/java/com/example/Notification/repository/PaymentReminderRepo.java
@@ -2,6 +2,8 @@ package com.ezpay.notifications.repo;
 
 import com.ezpay.notifications.model.PaymentReminder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
