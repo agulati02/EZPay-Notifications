5e675006929dcd1a1759667a2aeba79460fb668f
Anurag Gulati <anuraggulati1902@gmail.com>
2024-09-05 16:39:23 +0530

    Add files via upload

---
+++ Notification/src/main/java/com/example/Notification/model/Transaction.java
@@ -0,0 +1,81 @@
+package com.example.Notification.model;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+/**
+ * Transaction class
+ * Represents a financial transaction entity.
+ * 
+ * @author Akhil Kholia
+ * @date 2024-08-31
+ */
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "transaction_seq")
+    @SequenceGenerator(name = "transaction_seq", sequenceName = "TRANSACTION_SEQ", allocationSize = 1)
+    private Long id;
+    private Long userId;
+    private String type;
+    private double  amount;
+    private Timestamp dateOfTransaction;
+    private String status;
+    
+    public Transaction() {}
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double  getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double  amount) {
+        this.amount = amount;
+    }
+
+    public Timestamp getDate() {
+        return dateOfTransaction;
+    }
+
+    public void setDate(Timestamp dateOfTransaction) {
+        this.dateOfTransaction = dateOfTransaction;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+    	this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+    
+    public void setUserId(Long userId) {
+    	this.userId = userId;
+    }
+
+    
+}
