1b76e4e60e0b95b4447f2111abfae4e12be7fdf8
Anurag Gulati <anuraggulati1902@gmail.com>
2024-09-18 16:05:40 +0530

    Update BubblesBackground.js Documented

--- ezpay_notifications/src/components/BubblesBackground.js
+++ ezpay_notifications/src/components/BubblesBackground.js
@@ -1,22 +1,28 @@
 import '../assets/styles/bg.scss';
 
+/**
+ * A React component that renders an animated background of floating bubbles.
+ * 
+ * @component
+ * @returns {JSX.Element} The rendered background with animated bubbles
+ */
 const BubblesBackground = () => {
     return (
-        <div class="bubbles">
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
+        <div className="bubbles">
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
         </div>
-    )
-}
+    );
+};
 
 export default BubblesBackground;
