a35eb0d3248dc17453017b5649c9c0ec26bea78f
Jai Singh <78266589+Jai3-ui@users.noreply.github.com>
2024-09-19 08:33:08 +0530

    Update NotificationTitle.js

--- ezpay_notifications/src/components/NotificationTitle.js
+++ ezpay_notifications/src/components/NotificationTitle.js
@@ -1,9 +1,23 @@
-import '../assets/styles/NotificationTitle.css';
+// src/Component/NotificationTitle.js
 
+// Author Names: Akhil Kholia, Jai Singh, Geethapriya T
+// Date: 14/09/2024
+
+import '../assets/styles/NotificationTitle.css'; // Import custom CSS for styling the notification title
+
+/**
+ * A simple functional component that displays the title for the notifications section.
+ * 
+ * External Modules:
+ * - NotificationTitle.css: Custom styling for the notification title.
+ * 
+ * @component
+ * @returns {JSX.Element} The rendered notification title.
+ */
 const NotificationTitle = () => {
     return (
         <>
-            <div class="notification-title">
+            <div className="notification-title">
                 <h3>Notifications</h3>
             </div>
         </>
